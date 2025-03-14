2dd8b7e2248ec5bad3b7e9749d89a1fa20a0171e
Daniel Peñaloza <juandanielpenalozabrito@gmail.com>
2025-03-14 12:09:10 -0500

    Upload basic linting and styles configurations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,36 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        sans: [ 'var(--font-lato)' ],
+        playfair: [ 'var(--font-playfair)' ]
       },
-    },
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        calypso: {
+          50: '#eefbfd',
+          100: '#d4f4f9',
+          200: '#aee8f3',
+          300: '#76d6ea',
+          400: '#36bada',
+          500: '#1b9dbf',
+          600: '#197fa1',
+          DEFAULT: '#1c6a88',
+          800: '#1f546b',
+          900: '#1e475b',
+          950: '#0e2d3e'
+        }
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
