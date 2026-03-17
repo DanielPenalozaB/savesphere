f9f28a55e55a436a0566fd4dc70fec43e2be30e8
Daniel Peñaloza <juandanielpenalozabrito@gmail.com>
2026-03-17 08:45:36 -0500

    fix: Update income-vs-expenses component

--- apps/web/src/lib/components/dashboard/income-vs-expenses.svelte
+++ apps/web/src/lib/components/dashboard/income-vs-expenses.svelte
@@ -47,47 +47,45 @@
   <DashboardCard.Content>
     <div class="flex items-start justify-between">
       <div class="flex flex-col">
-        <DashboardCard.Title>
-          Income vs Expenses
-        </DashboardCard.Title>
+        <DashboardCard.Title>Income vs Expenses</DashboardCard.Title>
         <DashboardCard.Description>Last 4 months</DashboardCard.Description>
       </div>
-    <button class="text-slate-500 transition-colors hover:text-white">
-      <Info class="size-5" />
-    </button>
-  </div>
-  <div
-    class="[&_.lc-axis-tick-label]:fill-muted-foreground [&_.lc-axis-tick-label]:stroke-transparent dark:[&_.lc-axis-tick-label]:fill-white [&_.lc-bar]:stroke-transparent [&_.lc-bar]:stroke-0 [&_.lc-legend-swatch-label]:text-muted-foreground dark:[&_.lc-legend-swatch-label]:text-white [&_text]:stroke-transparent"
-  >
-    <BarChart
-      {data}
-      x="month"
-      series={[
-        {
-          key: 'Income',
-          color: 'var(--color-indigo-500)'
-        },
-        {
-          key: 'Expenses',
-          color: 'var(--color-violet-500)'
-        },
-        {
-          key: 'Savings',
-          color: 'var(--color-purple-500)'
-        }
-      ]}
-      seriesLayout="group"
-      props={{
-        xAxis: { format: 'none' },
-        yAxis: { format: 'metric' },
-        tooltip: {
-          header: { format: 'none' }
-        }
-      }}
-      legend
-      height={300}
-      groupPadding={0.2}
-    />
-  </div>
+      <button class="text-slate-500 transition-colors hover:text-white">
+        <Info class="size-5" />
+      </button>
+    </div>
+    <div
+      class="[&_.lc-axis-tick-label]:fill-muted-foreground [&_.lc-axis-tick-label]:stroke-transparent dark:[&_.lc-axis-tick-label]:fill-white [&_.lc-bar]:stroke-transparent [&_.lc-bar]:stroke-0 [&_.lc-legend-swatch-label]:text-muted-foreground dark:[&_.lc-legend-swatch-label]:text-white [&_text]:stroke-transparent"
+    >
+      <BarChart
+        {data}
+        x="month"
+        series={[
+          {
+            key: 'Income',
+            color: 'var(--color-indigo-500)'
+          },
+          {
+            key: 'Expenses',
+            color: 'var(--color-violet-400)'
+          },
+          {
+            key: 'Savings',
+            color: 'var(--color-purple-500)'
+          }
+        ]}
+        seriesLayout="group"
+        props={{
+          xAxis: { format: 'none' },
+          yAxis: { format: 'metric' },
+          tooltip: {
+            header: { format: 'none' }
+          }
+        }}
+        legend
+        height={300}
+        groupPadding={0.2}
+      />
+    </div>
   </DashboardCard.Content>
 </DashboardCard.Root>
